9a4a35f13ae2cdb8154920afe58862ebff782908
Cameron Brouk <cameronbrouk@gmail.com>
2021-06-22 15:30:26 -0500

    add TodoList component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Container } from '@material-ui/core'
+import { TodoList } from './TodoList/components/TodoList'
 
 function App() {
   return (
     <div>
-      <header className='App-header'></header>
+      <header>
+        <h1>Drive Social Media Interview - TODO App</h1>
+      </header>
+
+      <Container maxWidth='sm'>
+        <TodoList />
+      </Container>
     </div>
   )
 }
