90107a1bc727af390c8e902501d46f3dacd0172c
Cameron Brouk <cameronbrouk@gmail.com>
2021-06-22 15:19:47 -0500

    add skeleton components for todoList and todoItem

---
+++ src/TodoList/components/TodoItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Todo as Todo } from '../todoList.types'
+
+type Props = {
+  todo: Todo
+}
+
+export const TodoItem = (props: Props) => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
