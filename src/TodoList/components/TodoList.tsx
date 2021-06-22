e726c200ab2816d4ef991bab43e272668e39170e
Cameron Brouk <cameronbrouk@gmail.com>
2021-06-22 15:31:20 -0500

    render existing Todos

--- src/TodoList/components/TodoList.tsx
+++ src/TodoList/components/TodoList.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useTodos } from '../hooks/useTodos'
+import { TodoItem } from './TodoItem'
 
 export const TodoList = () => {
+  const { clearTodos, createTodo, todoList, removeTodo, updateTodo } = useTodos(
+    [],
+  )
+
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className='todo-list'>
+      {todoList.map((todo, i) => (
+        <div key={todo.id} className='todo-item'>
+          <TodoItem todo={todo} />
+        </div>
+      ))}
     </div>
   )
 }
