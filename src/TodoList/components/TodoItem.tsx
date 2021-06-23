d06b746e8e1ec32ed46c5fe4dec797286a0f5119
Cameron Brouk <cameronbrouk@gmail.com>
2021-06-22 22:03:39 -0500

    add checkbox, deletion, and title to TodoItem component

--- src/TodoList/components/TodoItem.tsx
+++ src/TodoList/components/TodoItem.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import { Todo as Todo } from '../todoList.types'
+import { Todo } from '../todoList.types'
+import { Checkbox, IconButton, Icon } from '@material-ui/core'
+import CancelIcon from '@material-ui/icons/Cancel'
+import { updateTypePredicateNodeWithModifier } from 'typescript'
 
 type Props = {
   todo: Todo
   removeTodo: (id: string) => void
-  toggleCompleted: (id: string) => void
+  updateTodo: (id: string, todo: Partial<Omit<Todo, 'id'>>) => void
 }
 
-export const TodoItem = (props: Props) => {
+export const TodoItem = ({ todo, removeTodo, updateTodo }: Props) => {
+  const toggleTodo = () => {
+    updateTodo(todo.id, { ...todo, completed: !todo.completed })
+  }
+
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className='todo-item'>
+      <Checkbox
+        color='primary'
+        checked={todo.completed}
+        onChange={toggleTodo}
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+      <p>{todo.title}</p>
+
+      <div className='remove-icon'>
+        <IconButton color='secondary' onClick={() => removeTodo(todo.id)}>
+          <CancelIcon />
+        </IconButton>
+      </div>
     </div>
   )
 }
