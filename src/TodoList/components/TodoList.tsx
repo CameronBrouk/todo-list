fb6c3c84cc3c1a45e5331245b9bdf817565ebcf5
Cameron Brouk <cameronbrouk@gmail.com>
2021-06-22 22:04:32 -0500

    implement search functionality for todo list

--- src/TodoList/components/TodoList.tsx
+++ src/TodoList/components/TodoList.tsx
@@ -1,42 +1,43 @@
 import { Button, TextField, Modal } from '@material-ui/core'
 import React, { useState } from 'react'
 import { useTodos } from '../hooks/useTodos'
+import { searchObjArray } from '../todoList.helpers'
 import { CreateTodoModal } from './CreateTodoModal'
 import { TodoItem } from './TodoItem'
 
 export const TodoList = () => {
-  const { clearTodos, createTodo, todoList, removeTodo, updateTodo, getTodo } =
-    useTodos([])
-
+  const { createTodo, todoList, removeTodo, updateTodo } = useTodos([])
   const [createModalOpen, setCreateModalOpen] = useState(false)
-
-  const toggleCompleted = (id: string) => {
-    const todo = getTodo(id)
-    if (!todo) return
-    updateTodo(id, {
-      completed: !todo.completed,
-    })
-  }
+  const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <div className='todo-list'>
-      <TextField name='search' autoComplete='off' label='Search Todos' />
+      <TextField
+        name='search'
+        autoComplete='off'
+        autoFocus
+        color='primary'
+        label='Search Todos'
+        onChange={({ target }) => setSearchTerm(target.value)}
+      />
 
-      {todoList.map((todo, i) => (
-        <div key={todo.id} className='todo-item'>
+      <div className='todos'>
+        {searchObjArray(todoList, searchTerm).map(todo => (
           <TodoItem
             todo={todo}
             removeTodo={removeTodo}
-            toggleCompleted={toggleCompleted}
+            updateTodo={updateTodo}
           />
-        </div>
-      ))}
+        ))}
+      </div>
 
       <div className='todo-action-buttons'>
-        <Button variant='outlined' onClick={() => setCreateModalOpen(true)}>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => setCreateModalOpen(true)}>
           Create Todo
         </Button>
-        <Button variant='outlined'>Clear Todos</Button>
       </div>
 
       <CreateTodoModal
